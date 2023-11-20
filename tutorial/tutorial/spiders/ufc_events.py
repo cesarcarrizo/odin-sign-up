03d50114c93fd00fd4b366862555b458353ddb60
Cesar Carrizo <cesarcarrizo95@gmail.com>
2023-11-20 17:11:42 +0000

    ufc events tutorial progression

--- tutorial/tutorial/spiders/ufc_events.py
+++ tutorial/tutorial/spiders/ufc_events.py
@@ -1,14 +1,19 @@
 import scrapy
+from scrapy.linkextractors import LinkExtractor
 
 
 class UfcEventsSpider(scrapy.Spider):
     name = "ufc_events"
     allowed_domains = ["ufcstats.com"]
-    start_urls = ["http://ufcstats.com/statistics/events/completed"]
+    start_urls = ["http://ufcstats.com/statistics/events/completed?page=all"]
 
     def parse(self, response):
        events_links = response.xpath("//i//a/@href")
+    #    events_links = [link.url for link in LinkExtractor().extract_links(response)]
        yield from response.follow_all(events_links, self.parse_event)
+
+    #    pagination_links = response.xpath('(//ul[@class="b-statistics__paginate"]//li//a)[2]')
+    #    yield from response.follow_all(pagination_links, self.parse)
 
     def parse_event(self, response):
         def extract_with_xpath(expression):
@@ -18,5 +23,5 @@ class UfcEventsSpider(scrapy.Spider):
         yield {
             "event_name": extract_with_xpath("//div//h2//span/text()"),
             "event_date": extract_with_xpath("(//li[contains(@class, 'b-list__box-list-item')]/text())[2]"),
-            "event_location": extract_with_xpath("(//li[contains(@class, 'b-list__box-list-item')]/text())[4]")
+            "event_location": extract_with_xpath("(//li[contains(@class, 'b-list__box-list-item')]/text())[4]"),
         }
